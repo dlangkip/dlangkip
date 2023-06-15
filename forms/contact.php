c2d338b247a295bd8c55d76cb4d2c7e8d0f7ceb2
Dlangkip <124573143+Dlangkip@users.noreply.github.com>
2023-06-15 09:56:18 +0300

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -1,19 +1,32 @@
 <?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  // Get the form fields and sanitize the data
+  $name = htmlspecialchars($_POST["name"]);
+  $email = htmlspecialchars($_POST["email"]);
+  $subject = htmlspecialchars($_POST["subject"]);
+  $message = htmlspecialchars($_POST["message"]);
 
-$name = $_POST['name'];
-$email = $_POST['email'];
-$subject = $_POST['subject'];
-$message = $_POST['message'];
+  // Set up the email recipient
+  $to = "dlangkipro@gmail.com";
 
-$mailheader = "From:" .$name."<".$email.">\r\n";
+  // Set up the email headers
+  $headers = "From: $email" . "\r\n";
+  $headers .= "Reply-To: $email" . "\r\n";
+  $headers .= "MIME-Version: 1.0" . "\r\n";
+  $headers .= "Content-type: text/html; charset=UTF-8" . "\r\n";
 
-$recepient = "amoskiprotich1130@gmail.com";
+  // Construct the email body
+  $email_body = "<h3>New Contact Message</h3>";
+  $email_body .= "<p><strong>Name:</strong> $name</p>";
+  $email_body .= "<p><strong>Email:</strong> $email</p>";
+  $email_body .= "<p><strong>Subject:</strong> $subject</p>";
+  $email_body .= "<p><strong>Message:</strong> $message</p>";
 
-mail($recepient, $subject, $message, $mailheader)
-or die("Error!");
-
-echo "message sent";
-
-
-
+  // Send the email
+  if (mail($to, $subject, $email_body, $headers)) {
+    echo "success";
+  } else {
+    echo "error";
+  }
+}
 ?>
